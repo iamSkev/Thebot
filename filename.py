f5f5ef9249719ae5ece779d59c51fc3a8bf6e565
iamSkev <77540541+iamSkev@users.noreply.github.com>
2021-01-17 00:15:50 +0800

    Add files via upload

---
+++ filename.py
@@ -0,0 +1,44 @@
+import discord
+import random
+import asyncio
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('Ping'):
+            await message.channel.send('Pong {0.author.mention}'.format(message))
+        if message.content.startswith('retard'):
+        	await message.channel.send('no u bitch')
+  
+
+
+        if message.content.startswith('Guess'):
+            await message.channel.send('Guess a number between 1 and 10.')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send('Sorry, you took too long it was {}.'.format(answer))
+
+            if int(guess.content) == answer:
+                await message.channel.send('You are right!')
+            else:
+                await message.channel.send('Oops. It is actually {}.'.format(answer))
+
+
+client = MyClient()
+client.run('Nzk5NzczMzIyNzE5MjY0ODg4.YAIc8w.zywx_5UZ1HBPbkB-WjGE4IqSMZE')
